32dab4a5c4928d5471375d0960b308c2d037352f
Mateusz Urban <26363084+MateuszUrb@users.noreply.github.com>
2023-10-08 01:43:42 +0200

    feat: add navigation links to main page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,37 @@
 import { Routes } from "@config/routes";
+import Link from "next/link";
+import Image from "next/image";
 import styles from "./index.module.scss";
 
 const IssuesPage = () => {
   return (
     <div>
+      {" "}
       <header className={styles.header}>
+        <Image
+          width={118}
+          height={33}
+          src="/icons/logo-large.svg"
+          alt="Prolog logo"
+        />
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <nav className={styles.header_nav}>
+          <ul className={styles.navList}>
+            <li className={styles.nav_link}>
+              <Link href="/">Home</Link>
+            </li>
+            <li className={styles.nav_link}>
+              <Link href="#">Products</Link>
+            </li>
+            <li className={styles.nav_link}>
+              <Link href="#">Documentation</Link>
+            </li>
+            <li className={styles.nav_link}>
+              <Link href="#">Pricing</Link>
+            </li>
+          </ul>
+        </nav>
+        <Link href={Routes.projects}>Open Dashboard</Link>
       </header>
       <button
         className={styles.contactButton}
